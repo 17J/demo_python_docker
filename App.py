4de1897ed9a2b20114a33396501484e4e94a4747
Rahul Joshi <48386687+17J@users.noreply.github.com>
2024-02-29 13:39:41 +0530

    changes content

--- App.py
+++ App.py
@@ -1,6 +1,1 @@
-first_value = int(input("Enter The First Value : "))
-second_value = int(input("Enter The Second Value : "))
-
-total = first_value + second_value
-
-print(total)
+print("Hello World")
